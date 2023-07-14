3c947ec91eeee01bb390f1c805f397af99713269
Gulalam <khangulalam.k@gmail.com>
2023-07-14 10:13:50 +0500

    main comment deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'homePage/home_page.dart';
 
-// import 'package:grocery_app/variables.dart';
-// flutter awesome snipits
 void main() {
   runApp(const MyApp());
 }
